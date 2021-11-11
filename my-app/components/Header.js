9073c6f20237a2a6cbd598aa2a922c79412a532e
dgadhia <gadhiad2@gmail.com>
2021-11-11 22:27:24 +0800

    Enter

--- my-app/components/Header.js
+++ my-app/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>Next.js With NodeJS</h1>
+            <h1>Next.js With NodeJSSSSSSS</h1>
         </div>
     )
 }
